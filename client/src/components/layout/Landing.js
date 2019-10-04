e54f3c51dc2d14887706ae3d6eee05278c0146a3
Thong Phan <phanhongthong@gmail.com>
2019-10-04 14:00:55 +0700

    fix deploy heroku

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -12,7 +12,7 @@ const Landing = ({ isAuthenticated }) => {
         <section className="landing">
             <div className="dark-overlay">
                 <div className="landing-inner">
-                    <h1 className="x-large">Welcome to Developer Profiles</h1>
+                    <h1 className="x-large">Developer Profiles</h1>
                     <p className="lead">
                         Create a developer profile / portfolio
                     </p>

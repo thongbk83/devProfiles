14291aaedfca1e44eab982a722fdb59228a03e5e
Thong Phan <phanhongthong@gmail.com>
2019-10-06 08:25:34 +0700

    update host server config

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,13 +1,6 @@
 const proxy = require("http-proxy-middleware");
 
 module.exports = function(app) {
-    if (process.env.NODE_ENV !== "production") {
-        app.use(proxy("/api/*", { target: "http://localhost:5000" }));
-    } else {
-        app.use(
-            proxy("/api/*", {
-                target: "https://thong-dev-profiles.herokuapp.com"
-            })
-        );
-    }
+    const hostServer = process.env.HOST || "http://localhost:5000";
+    app.use(proxy("/api/*", { target: hostServer }));
 };

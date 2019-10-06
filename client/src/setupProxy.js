6f63b28872d20fefda23af079448ef19218b6505
Thong Phan <phanhongthong@gmail.com>
2019-10-06 09:09:16 +0700

    travis 2

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,6 +1,6 @@
 const proxy = require("http-proxy-middleware");
 
 module.exports = function(app) {
-    const hostServer = process.env.HOST || "http://localhost:5000";
+    const hostServer = "http://localhost:5000";
     app.use(proxy("/api/*", { target: hostServer }));
 };
